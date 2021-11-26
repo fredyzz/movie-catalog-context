8f1a1ca39d77cd99ea9cc72e31a13b8634b9e065
Alfredo Zimperz <alfredo.zimperz@adevinta.com>
2021-11-26 12:56:41 +0100

    Add Header & MovieList components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import Header from "./components/Header";
+import MovieList from "./components/Movie/MovieList";
 import "./App.css";
+
+// Mocks for developing purposes
+import movieListMock from "./__mocks__/movies";
+
+const movieList = movieListMock.result;
 
 const App = () => (
   <div className="App">
-    <h1>Movie catalog</h1>
+    <Header title="Movie Catalog" />
+    <MovieList movies={movieList} />
   </div>
 );
-
 export default App;
