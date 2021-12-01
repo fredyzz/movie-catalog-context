6d8f3d4a49197ce7972c8b369f8a4dce48fa2f23
Alfredo Zimperz <alfredo.zimperz@adevinta.com>
2021-12-01 14:25:39 +0100

    Implement ThemeToogle and add styles to Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,21 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import "./index.css";
+import ThemeToogle from "./ThemeToogle";
 
-const Header = ({ title, subTitle }) => {
+const Header = ({ isDarkMode, title, toogleDarkMode, subTitle }) => {
+  const baseClass = "Header";
+
   return (
-    <div>
-      <h1>{title}</h1>
-      {subTitle && <h2>{subTitle}</h2>}
+    <div className={baseClass}>
+      <div className={`${baseClass}-titles`}>
+        <Link to="/">
+          <h1>{title}</h1>
+        </Link>
+
+        {subTitle && <h2>{subTitle}</h2>}
+      </div>
+      <div className={`${baseClass}-actions`}>
+        <ThemeToogle onToogle={toogleDarkMode} isDarkMode={isDarkMode} />
+      </div>
     </div>
   );
 };
 
 Header.propTypes = {
+  isDarkMode: PropTypes.bool,
   title: PropTypes.string.isRequired,
+  toogleDarkMode: PropTypes.func.isRequired,
   subTitle: PropTypes.string
 };
 
 Header.defaultProps = {
+  isDarkMode: false,
   subTitle: null
 };
 
