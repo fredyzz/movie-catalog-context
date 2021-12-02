e015c0a1e837a1cfe51de7f796848a4737247f01
Alfredo Zimperz <alfredo.zimperz@adevinta.com>
2021-12-02 12:32:39 +0100

    Get movieId from URL params

--- src/components/Movie/MovieDetail/index.jsx
+++ src/components/Movie/MovieDetail/index.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import "./index.css";
 
 const baseClass = "MovieDetail";
 
 const MovieDetail = () => {
+  const { movieId } = useParams();
+
   return (
     <div className={baseClass}>
       <h3>Movie detail</h3>
+      <p>Movie Id: {movieId}</p>
     </div>
   );
 };
