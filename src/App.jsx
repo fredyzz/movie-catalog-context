0d6030456ff1bf21e09ef783a291edb083a4b876
Alfredo Zimperz <alfredo.zimperz@adevinta.com>
2021-12-02 14:04:56 +0100

    Implement GlobalContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import { Routes, Route } from "react-router-dom";
+import GlobalContext from "./GlobalContext";
 import Layout from "./components/Layout";
 import MovieList from "./components/Movie/MovieList";
 import MovieDetail from "./components/Movie/MovieDetail";
@@ -13,34 +14,41 @@ const movieList = movieListMock.result;
 const { REACT_APP_TITLE } = process.env;
 
 const App = () => {
+  // eslint-disable-next-line no-console
+  console.log("render App");
+
+  // Constructing context initial values and methods
   const [isDarkMode, setIsDarkMode] = useState(false);
   const toogleDarkMode = () => setIsDarkMode(!isDarkMode);
 
+  const contextInitialValues = {
+    isDarkMode,
+    toogleDarkMode
+  };
+
+  // This is to avoid re-render unless the values change
+  const memoizedContextInitialValues = useMemo(
+    () => contextInitialValues,
+    [isDarkMode, toogleDarkMode]
+  );
+
   const theme = isDarkMode ? "dark" : "light";
 
-  const getLayout = () => (
-    <Layout
-      isDarkMode={isDarkMode}
-      title={REACT_APP_TITLE}
-      toogleDarkMode={toogleDarkMode}
-    />
-  );
-
-  const getMovieList = () => (
-    <MovieList movies={movieList} isDarkMode={isDarkMode} />
-  );
-
+  const getLayout = () => <Layout title={REACT_APP_TITLE} />;
+  const getMovieList = () => <MovieList movies={movieList} />;
   const getMovieDetail = () => <MovieDetail />;
 
   return (
-    <div className={`App ${theme}`}>
-      <Routes>
-        <Route path="/" element={getLayout()}>
-          <Route index element={getMovieList()} />
-          <Route path="movie/:movieId" element={getMovieDetail()} />
-        </Route>
-      </Routes>
-    </div>
+    <GlobalContext.Provider value={memoizedContextInitialValues}>
+      <div className={`App ${theme}`}>
+        <Routes>
+          <Route path="/" element={getLayout()}>
+            <Route index element={getMovieList()} />
+            <Route path="movie/:movieId" element={getMovieDetail()} />
+          </Route>
+        </Routes>
+      </div>
+    </GlobalContext.Provider>
   );
 };
 export default App;
