427fff34ecda546c3f16d2b38270d32cf4bceb66
Alfredo Zimperz <alfredo.zimperz@adevinta.com>
2021-12-03 10:27:31 +0100

    Remove theme logic

--- src/components/Movie/MovieCard/index.jsx
+++ src/components/Movie/MovieCard/index.jsx
@@ -3,13 +3,15 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import MovieCardFront from "./MovieCardFront";
 import MovieCardBack from "./MovieCardBack";
+
 import { MOVIE_IMAGE_BASE_URL } from "../../../config";
 import "./index.css";
 
 const baseClass = "MovieCard";
 
-const MovieCard = ({ movie, isDarkMode }) => {
-  const theme = isDarkMode ? "dark" : "light";
+const MovieCard = ({ movie }) => {
+  // eslint-disable-next-line no-console
+  console.log("render MovieCard");
 
   const { overview, poster_path: posterPath, title } = movie;
   const moviePoster = `${MOVIE_IMAGE_BASE_URL}${posterPath}`;
@@ -26,7 +28,6 @@ const MovieCard = ({ movie, isDarkMode }) => {
           <MovieCardBack
             baseClass={baseClass}
             overview={overview}
-            theme={theme}
             title={title}
           />
         </div>
@@ -36,17 +37,12 @@ const MovieCard = ({ movie, isDarkMode }) => {
 };
 
 MovieCard.propTypes = {
-  isDarkMode: PropTypes.bool,
   movie: PropTypes.shape({
     id: PropTypes.number.isRequired,
     overview: PropTypes.string.isRequired,
     poster_path: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired
   }).isRequired
-};
-
-MovieCard.defaultProps = {
-  isDarkMode: false
 };
 
 export default MovieCard;
