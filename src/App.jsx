41755cc34a535c710305b24b1a93cdac97647426
Alfredo Zimperz <alfredo.zimperz@adevinta.com>
2021-12-03 10:26:07 +0100

    Remove theme logic from App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import MovieList from "./components/Movie/MovieList";
@@ -7,39 +7,27 @@ import "./App.css";
 
 // Mocks for developing purposes
 import movieListMock from "./__mocks__/movies";
+import { ThemeWrapper } from "./Context/Theme/ThemeWrapper";
 
 const movieList = movieListMock.result;
 
 const { REACT_APP_TITLE } = process.env;
 
 const App = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-  const toogleDarkMode = () => setIsDarkMode(!isDarkMode);
-
-  const theme = isDarkMode ? "dark" : "light";
-
-  const getLayout = () => (
-    <Layout
-      isDarkMode={isDarkMode}
-      title={REACT_APP_TITLE}
-      toogleDarkMode={toogleDarkMode}
-    />
-  );
-
-  const getMovieList = () => (
-    <MovieList movies={movieList} isDarkMode={isDarkMode} />
-  );
-
+  const getLayout = () => <Layout title={REACT_APP_TITLE} />;
   const getMovieDetail = () => <MovieDetail />;
+  const getMovieList = () => <MovieList movies={movieList} />;
 
   return (
-    <div className={`App ${theme}`}>
-      <Routes>
-        <Route path="/" element={getLayout()}>
-          <Route index element={getMovieList()} />
-          <Route path="movie/:movieId" element={getMovieDetail()} />
-        </Route>
-      </Routes>
+    <div className="App">
+      <ThemeWrapper>
+        <Routes>
+          <Route path="/" element={getLayout()}>
+            <Route index element={getMovieList()} />
+            <Route path="movie/:movieId" element={getMovieDetail()} />
+          </Route>
+        </Routes>
+      </ThemeWrapper>
     </div>
   );
 };
